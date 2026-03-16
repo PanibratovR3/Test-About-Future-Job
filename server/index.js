052bf79edd9c6603154e69d3d1be74cce4e19286
PanibratovR3 <roman.panibratov@gmail.com>
2026-03-16 17:39:15 +0200

    Restored previous changes.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const applicantRouter = require("./routes/applicantRouter");
+require("dotenv").config({ quiet: true });
 
 const app = express();
 
