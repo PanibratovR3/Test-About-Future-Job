5e706f15469e7ebb429f37d1eefd7d53ce114f20
PanibratovR3 <roman.panibratov@gmail.com>
2026-03-16 17:38:17 +0200

    Removed dotenv from index.js module.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const applicantRouter = require("./routes/applicantRouter");
-require("dotenv").config({ quiet: true });
 
 const app = express();
 
