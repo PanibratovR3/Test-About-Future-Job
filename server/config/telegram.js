5020c34a3e21276414eb0883b31edffcb13151d6
PanibratovR3 <roman.panibratov@gmail.com>
2026-03-20 15:32:50 +0200

    Replaced telegram package with teleproto.

--- server/config/telegram.js
+++ server/config/telegram.js
@@ -1,5 +1,5 @@
-const { TelegramClient } = require("telegram");
-const { StringSession } = require("telegram/sessions");
+const { TelegramClient } = require("teleproto");
+const { StringSession } = require("teleproto/sessions");
 require("dotenv").config({ quiet: true });
 
 const SESSION = new StringSession("");
